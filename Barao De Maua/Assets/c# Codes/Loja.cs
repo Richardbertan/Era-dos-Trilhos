9fa98994b0afb139bb89d01d2f8b0d45888bd68e
Richardbertan <Richardbertan18@gmail.com>
2023-09-21 11:41:36 -0300

    21 /09 Pc Escola

--- Barao De Maua/Assets/c# Codes/Loja.cs
+++ Barao De Maua/Assets/c# Codes/Loja.cs
@@ -5,20 +5,26 @@ using UnityEngine.UI;
 
 public class Loja : MonoBehaviour
 {
-    static int dinheiro;
+    public static int dinheiro;
     int lastMoney = 0;
     [SerializeField] Text dinhTxt;  
     static float increaseRate;
+    public static int Click;
     
     void FixedUpdate ()
     {
         dinheiro = (int) increaseRate;
-        increaseRate += (madeira.increaseRate + pedra.increaseRate + carvao.increaseRate + ferro.increaseRate + ouro.increaseRate + diamante.increaseRate) * Time.deltaTime;
+        dinheiro += Click;
+        increaseRate += ((madeira.increaseRate + pedra.increaseRate + carvao.increaseRate + ferro.increaseRate + ouro.increaseRate + diamante.increaseRate) * Time.deltaTime);
         dinhTxt.text = dinheiro.ToString();
             if (dinheiro > lastMoney)
             {
                 lastMoney = dinheiro;
                 print("almentou");
+            }
+            if (Click > 0)
+            {
+                Click = 0;
             }
     }
 }
--- Barao De Maua/Assets/c# Codes/Recurcos/madeira.cs
+++ Barao De Maua/Assets/c# Codes/Recurcos/madeira.cs
@@ -6,4 +6,9 @@ using UnityEngine.UI;
 public class madeira : MonoBehaviour
 {  
     public static float increaseRate = 1f;
+
+    public void OnClick ()
+    {
+        Loja.Click += 10;
+    }
 }
