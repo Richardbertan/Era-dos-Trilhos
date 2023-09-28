884f10e60266f06a89c332588574d95863edc9dd
Richardbertan <Richardbertan18@gmail.com>
2023-09-28 11:43:15 -0300

    Atualização gameplay

--- Barao De Maua/Assets/c# Codes/Upgrades/UpgradeStone.cs
+++ Barao De Maua/Assets/c# Codes/Upgrades/UpgradeStone.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeStone : MonoBehaviour
 {
    public static int Price = 100;
+    [SerializeField] 
+    Text PriceTxt;
+    void Update ()
+    {
+    PriceTxt.text = Price.ToString();
+    }
    public void OnClick ()
    {
         if (Loja.dinheiro >= Price)
