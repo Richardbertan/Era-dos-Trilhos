dd84617ecd0d54552bbc19bef810270d82af871c
Richardbertan <Richardbertan18@gmail.com>
2023-10-25 15:04:17 -0300

    att fim do jogo

--- Barao De Maua/Assets/c# Codes/Loja.cs
+++ Barao De Maua/Assets/c# Codes/Loja.cs
@@ -8,11 +8,13 @@ public class Loja : MonoBehaviour
     public static int dinheiro;
     [SerializeField] Text dinhTxt;  
     public static float dinFloat;
+    [SerializeField] int Cheat;
     public static float Click;
     [SerializeField]
     GameObject Historia;
     [SerializeField]
     GameObject Fim;
+    static bool Cabo = true;
     void Start ()
     {
         Historia.SetActive(true);
@@ -20,6 +22,7 @@ public class Loja : MonoBehaviour
     void FixedUpdate ()
     {
         dinheiro = (int) dinFloat;
+        dinFloat += Cheat;
         dinFloat += ((madeira.increaseRate + pedra.increaseRate + carvao.increaseRate + ferro.increaseRate + ouro.increaseRate + diamante.increaseRate) * Time.deltaTime);
         dinhTxt.text = dinheiro.ToString();
             if (Click > 0)
@@ -27,9 +30,10 @@ public class Loja : MonoBehaviour
                 dinFloat += Click;
                 Click = 0;
             }
-            if (dinheiro >= 5000000)
+            if (dinheiro >= 500000 && Cabo)
             {
                 Fim.SetActive(true);
+                Cabo = false;
             }
     }
 
