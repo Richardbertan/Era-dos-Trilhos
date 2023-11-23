c9f3d07e76f8142fff51316088378d47320e7a7b
Richardbertan <Richardbertan18@gmail.com>
2023-11-23 09:27:48 -0300

    att dia 23 /11 machado certo

---
+++ Barao De Maua/Assets/c# Codes/TremCaiu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TremCaiu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Box;
+    // Update is called once per frame
+    void OnBecameVisible()
+    {
+        print("é isso");
+        if (false)
+        {
+
+        }
+    }
+}
